7fcaef66bdaedeab4af88479afa00254375b91ed
raushankit <raushankit160226@gmail.com>
2023-06-28 01:34:59 +0530

    incept

---
+++ src/main/java/com/raushankit/boilers/BoilersApplication.java
@@ -0,0 +1,13 @@
+package com.raushankit.boilers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raushankit/boilers/BoilersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raushankit.boilers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
